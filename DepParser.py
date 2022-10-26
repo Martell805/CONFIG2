9b508cdd644c07290e57784373076486e69acdf6
Martell <vovasamb@yandex.ru>
2022-10-26 20:34:51 +0300

    Initial commit

---
+++ DepParser.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+from functools import cache
+
+
+class DepParser:
+    @staticmethod
+    @cache
+    def __parse(package_name: str, tab_level: int = 1) -> None:
+        request = requests.get(f"https://www.npmjs.com/package/{package_name}/?activeTab=dependencies").content.decode()
+        soup = BeautifulSoup(request, features="lxml")
+
+        deps = soup.find("ul", {"aria-label": "Dependencies"})
+
+        if deps is None:
+            return
+
+        for dep in deps.find_all("a"):
+            dep = dep.text
+            print("\t" * tab_level, package_name, " -> ", dep, sep="")
+            DepParser.__parse(dep, tab_level + 1)
+
+    @staticmethod
+    def parse(package_name: str) -> None:
+        print("Digraph {")
+        DepParser.__parse(package_name)
+        print("}")
---
+++ main.py
@@ -0,0 +1,4 @@
+from DepParser import DepParser
+
+if __name__ == '__main__':
+    DepParser.parse(input())  # express
